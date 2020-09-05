5182fc958e3542651dc7b3b46b15faf866ba78d7
Marcus <Zhuo.Huang@pccw.com>
2020-09-05 16:18:13 +0800

    Update Category.java

--- ForgeWurst MC 1.12.2/src/shared/java/net/wurstclient/forge/Category.java
+++ ForgeWurst MC 1.12.2/src/shared/java/net/wurstclient/forge/Category.java
@@ -9,13 +9,13 @@ package net.wurstclient.forge;
 
 public enum Category
 {
-	BLOCKS("Blocks"),
-	MOVEMENT("Movement"),
-	COMBAT("Combat"),
-	RENDER("Render"),
-	CHAT("Chat"),
-	FUN("Fun"),
-	OTHER("Other");
+	BLOCKS("方块"),
+	MOVEMENT("移动"),
+	COMBAT("战斗"),
+	RENDER("渲染"),
+	CHAT("聊天"),
+	FUN("功能"),
+	OTHER("其他");
 	
 	private final String name;
 	
